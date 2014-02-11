68382ca6b96907c27d88e0a4eb772b70665dd9fc
Angus Mark <ngsmrk@gmail.com>
2014-02-11 22:08:59 +0000

    Whitespace police

--- client/main.js
+++ client/main.js
@@ -1,35 +1,35 @@
 if (Meteor.isClient) {
-  Meteor.startup(function () {
-    console.log("Red stapler client started");
-  });  
-  
-  Template.officelist.offices = function () {
-    return Offices.find({}, {sort: {name: 1}});
-  };
+    Meteor.startup(function () {
+        console.log("Red stapler client started");
+    });
 
-  Template.officelist.selected_name = function () {
-    var office = Offices.findOne(Session.get("selected_office"));
-    return office && office.name;
-  };
+    Template.officelist.offices = function () {
+        return Offices.find({}, {sort: {name: 1}});
+    };
 
-  Template.office.selected = function () {
-    return Session.equals("selected_office", this._id) ? "selected" : '';
-  };
+    Template.officelist.selected_name = function () {
+        var office = Offices.findOne(Session.get("selected_office"));
+        return office && office.name;
+    };
 
-  Template.officelist.events({
-    'click input.inc': function () {
-      console.log("Selected office: " + Session.get("selected_office"));
-      selected_office = Offices.findOne(Session.get("selected_office"));
-      rows = selected_office.rows;
-      rows.push({"number": rows.length + 1});
-      Offices.update(Session.get("selected_office"), {$set: {rows: rows}});
-    }
-  });
+    Template.office.selected = function () {
+        return Session.equals("selected_office", this._id) ? "selected" : '';
+    };
 
-  Template.office.events({
-    'click span.name': function () {
-      console.log("Office click: " + this._id);
-      Session.set("selected_office", this._id);
-    }
-  });
+    Template.officelist.events({
+        'click input.inc': function () {
+            console.log("Selected office: " + Session.get("selected_office"));
+            selected_office = Offices.findOne(Session.get("selected_office"));
+            rows = selected_office.rows;
+            rows.push({"number": rows.length + 1});
+            Offices.update(Session.get("selected_office"), {$set: {rows: rows}});
+        }
+    });
+
+    Template.office.events({
+        'click span.name': function () {
+            console.log("Office click: " + this._id);
+            Session.set("selected_office", this._id);
+        }
+    });
 }
